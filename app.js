0b99301427c0156c1b33a5f04616c7b918a84876
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2020-06-24 02:03:18 +0530

    device

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/default.html')))
 
 http.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
+var clients = 0;
+
 io.on('connection', (socket) => {
+    clients++;
     console.log('a user connected');
+    socket.on('orientation', (alpha) => {
+        console.log('message: ' + alpha);
+        socket.broadcast.emit('change-o', alpha);
+    });
+    socket.on('disconnect', function () {
+        clients--;
+        io.sockets.emit('clients connected: ' + clients);
+    });
 });
--- public/flow.js
+++ public/flow.js
@@ -1,12 +1,13 @@
 
-var alpha = 0; // Orientation around Z axis
-var beta = 0; // Orientation around X axis
+var alpha = 1; // Orientation around Z axis
+var beta = 50; // Orientation around X axis
 var gamma = 0; // Orientation around Y axis
 
 function setup() {
 	createCanvas(710, 400);
 	if (window.DeviceOrientationEvent) {
 		window.addEventListener('deviceorientation', onOrientationChange);
+		window.addEventListener("devicemotion", handleMotion, true);
 	}
 }
 
@@ -18,14 +19,36 @@ function draw() {
 	translate((width / 2) - 50, height / 2);
 	rotate(alpha);
 	//rotateX(-beta);
-	rect(0, 0, 50, 100);
+	rect(0, 0, beta, beta + 1);
 	pop();
 }
 
 function onOrientationChange(e) {
-	alpha = e.alpha;
+
+
+	if (e.alpha < alpha - 1 || e.alpha > alpha + 1) {
+		alpha = e.alpha;
+		console.log('now');
+		socket.emit('orientation', alpha)
+	}
 	beta = e.beta;
 	gamma = e.gamma;
 
-	console.log(alpha + " " + beta + " " + gamma);
+	//console.log(alpha + " " + beta + " " + gamma);
 }
+
+function handleMotion() {
+	beta = event.acceleration.x
+	console.log('alpha' + alpha + " beta: " + beta + " " + gamma);
+}
+jQuery(document).ready(function () {
+	socket.on('change-o', function (_alpha) {
+		console.log('received :' + _alpha)
+		alpha = _alpha;
+	});
+});
+
+
+screen.orientation.addEventListener("change", function (e) {
+	alert(screen.orientation.type + " " + screen.orientation.angle);
+}, false);
