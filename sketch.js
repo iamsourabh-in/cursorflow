7e83a7604d7cadfe36dc02a0631db902bc450a61
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2020-06-23 22:45:45 +0530

    Update to defocus

--- sketch.js
+++ sketch.js
@@ -1,14 +1,48 @@
+var shapeCircle = {
+  x: 100,
+  y: 100,
+  r: 20
+}
+var showDefocus = false;
+
+var bgColor = {
+  r: 0,
+  g: 0,
+  b: 0
+}
+
+var spotColor = {
+  r: 200,
+  g: 10,
+  b: 200,
+  a: 120
+}
+
+
 function setup() {
-    createCanvas(1366, 768);
+  createCanvas(1366, 768);
+  background(bgColor.r, bgColor.g, bgColor.b);
+}
+
+function draw() {
+  noStroke();
+  if (mouseIsPressed) {
+
+    fill(spotColor.r, spotColor.g, spotColor.b);
+    circle(shapeCircle.x, shapeCircle.y, shapeCircle.r);
   }
-  
-  function draw() {
-    if (mouseIsPressed) {
-        fill(100,0,255);
-    } else {
-      fill(100,0,255,10);
-      noStroke();
-    }
-    
-    rect(mouseX, mouseY, 80, 80);
-  }
+  spotColor.r = random(100, 255);
+  spotColor.b = random(100, 190);
+  spotColor.g = 0;
+  shapeCircle.x = random(0, 1366);
+  shapeCircle.y = random(0, 786);
+
+  fill(spotColor.r, spotColor.g, spotColor.b, spotColor.a);
+  circle(shapeCircle.x, shapeCircle.y, shapeCircle.r);
+}
+
+
+function showDefocus() {
+ 
+
+}
